70bd116ed2f44f3288316464e4fd7bea10bb7930
royroycat <royroycat@gmail.com>
2021-01-06 21:13:40 +0800

    BUGFIX : attribute region cannot be set to null

--- app/models/ark_trading_info.py
+++ app/models/ark_trading_info.py
@@ -11,7 +11,7 @@ def define_entity(db):
         date = Optional(datetime)
         direction = Required(str)
         ticker = Optional(str)
-        region = Optional(str)
+        region = Optional(str, nullable=True)
         cusip = Optional(str)
         company = Optional(str)
         shares = Required(Decimal)
